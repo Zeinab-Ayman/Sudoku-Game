e01f5e453b2f74567ba2af780b49ac730ae30836
Zeinab-Ayman <zeinabayman304@gmail.com>
2026-09-16 17:37:09 +0300

    Implement Sudoku Solver Class

--- Sudoku_Game/main.cpp
+++ Sudoku_Game/main.cpp
@@ -1,11 +1,14 @@
 #include "ConsoleUI.h"
+
 #include<iostream>
 #include "Board.h"
 using namespace std;
 int main() {
     /* ConsoleUI ui;
      ui.run();*/
-    Board b;
+
+    //test Board class
+    /*Board b;
     cout << "Is row 0 valid for value 5? " << b.isRowValid(0, 5) << endl;
         if (b.isRowValid(0, 5)) {
             cout << "Board is complete" << endl;
@@ -13,7 +16,13 @@ int main() {
         else {
             cout << "Board is not complete" << endl;
 
-        }
+        }*/
+
+
+     //test SudokuSolver class
+     /*Board b;
+     SudokuSolver solver;
+     bool ok = solver.solve(b);*/
             
     return 0;
 }
